e4cb8f5d89419d46e0c6de8380a7b16505c15e01
shubhdeeppaul71 <shubhdeeppaul71@gmail.com>
2024-09-30 14:11:14 +0530

    Initial Commit

---
+++ convex/auth.config.ts
@@ -0,0 +1,10 @@
+const authConfig = {
+  providers: [
+    {
+      domain: "https://neutral-grubworm-47.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ]
+};
+
+export default authConfig;
